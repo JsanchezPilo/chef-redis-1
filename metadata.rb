d083b33cfb592635ac572a979bb405ce95bdd2ba
JsanchezPilo <38760945+JsanchezPilo@users.noreply.github.com>
2018-09-22 20:35:11 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,6 @@ maintainer_email 'andrey@skopenko.net'
 license          'Apache-2.0'
 description      'Installs/Configures Redis daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/scopenco/chef-redis'
-issues_url 'https://github.com/scopenco/chef-redis/issues'
 version '0.1.1'
 
 supports 'amazon'
